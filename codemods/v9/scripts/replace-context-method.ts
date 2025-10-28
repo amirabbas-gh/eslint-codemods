272aadcd0c8685d01b26facacce51f1ba1fb15ee
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-10-29 01:20:46 +0330

    chore: finish replace context method development

--- codemods/v9/scripts/replace-context-method.ts
+++ codemods/v9/scripts/replace-context-method.ts
@@ -1,4 +1,4 @@
-import { type Edit, type SgRoot } from "codemod:ast-grep";
+import { type Edit, type SgRoot, parse } from "codemod:ast-grep";
 import type JS from "codemod:ast-grep/langs/javascript";
 
 export default async function transform(root: SgRoot<JS>): Promise<string> {
@@ -28,31 +28,74 @@ export default async function transform(root: SgRoot<JS>): Promise<string> {
   if (createRule) {
     let text = createRule.text();
 
-    const expressions = createRule.findAll({
+    let newRoot = parse("javascript", text).root();
+
+    const expressions = newRoot.findAll({
       rule: {
         kind: "call_expression",
         has: {
           kind: "member_expression",
-          has: {
-            kind: "identifier",
-            regex: "context",
-            pattern: "$IDENTIFIER",
-          },
+          pattern: "$IDENTIFIER.$PROPERTY",
         },
       },
     });
+
+    let newRootEdits = [];
+
+    const changeMap = {
+      getSource: "getText",
+      getSourceLines: "getLines",
+      getAllComments: "getAllComments",
+      getNodeByRangeIndex: "getNodeByRangeIndex",
+      getCommentsBefore: "getCommentsBefore",
+      getCommentsAfter: "getCommentsAfter",
+      getCommentsInside: "getCommentsInside",
+      getJSDocComment: "getJSDocComment",
+      getFirstToken: "getFirstToken",
+      getFirstTokens: "getFirstTokens",
+      getLastToken: "getLastToken",
+      getLastTokens: "getLastTokens",
+      getTokenAfter: "getTokenAfter",
+      getTokenBefore: "getTokenBefore",
+      getTokenByRangeStart: "getTokenByRangeStart",
+      getTokens: "getTokens",
+      getTokensAfter: "getTokensAfter",
+      getTokensBefore: "getTokensBefore",
+      getTokensBetween: "getTokensBetween",
+      parserServices: "parserServices",
+      getDeclaredVariables: "getDeclaredVariables",
+    };
+
     for (let expression of expressions) {
       let identifier = expression.getMatch("IDENTIFIER");
-      if (!identifier) continue;
-      edits.push(identifier.replace("contextSourceCode"));
+      let property = expression.getMatch("PROPERTY");
+      if (!identifier || !property) continue;
+      let propertyText = property.text();
+      if (Object.keys(changeMap).includes(propertyText)) {
+        newRootEdits.push(
+          property.replace(
+            changeMap[propertyText as keyof typeof changeMap] as string
+          )
+        );
+      } else if (propertyText == "getComments") {
+        newRootEdits.push(
+          expression.replace(
+            `contextSourceCode.getCommentsBefore() + contextSourceCode.getCommentsInside() + contextSourceCode.getCommentsAfter()`
+          )
+        );
+      }
+
+      newRootEdits.push(identifier.replace("contextSourceCode"));
     }
 
-    let newText = `{
+    text = newRoot.commitEdits(newRootEdits);
+
+    let newCreate = `{
         const contextSourceCode = context.sourceCode ?? context.getSourceCode();${text.substring(
           1,
           text.length
         )}`;
-    edits.push(createRule.replace(newText));
+    edits.push(createRule.replace(newCreate));
   }
 
   let newSource = rootNode.commitEdits(edits);
